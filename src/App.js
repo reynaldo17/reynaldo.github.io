243dcf27411ec9fc71bd334f25694b7d8fdcfc3c
reynaldo17 <reynaldoestrella16@gmail.com>
2023-03-07 16:52:57 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/reynaldo17/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github!
         </a>
       </header>
     </div>
